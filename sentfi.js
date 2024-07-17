569e36e50f81d93ae1e3fc6d2d01f15115f2b263
david-aguiyi <davidaguiyi9@gmail.com>
2024-07-17 03:16:25 -0700

    dfsdf

--- sentfi.js
+++ sentfi.js
@@ -10,16 +10,72 @@ document.addEventListener("DOMContentLoaded", function () {
 
 let lastScrollY = window.scrollY;
 
-window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar');
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
 
-    if (window.scrollY < lastScrollY) {
-        // Scrolling up
-        navbar.classList.add('visible');
+    if (window.scrollY === 0) {
+        // At the top of the page
+        navbar.classList.remove("visible");
     } else {
-        // Scrolling down
-        navbar.classList.remove('visible');
+        // Not at the top of the page
+        navbar.classList.add("visible");
     }
 
     lastScrollY = window.scrollY;
 });
+
+// Optional: If you want the navbar to become visible if the user reloads the page and is not at the top
+window.addEventListener("load", () => {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 0) {
+        navbar.classList.add("visible");
+    }
+});
+
+
+
+// Optional: If you want the navbar to become visible if the user reloads the page and is not at the top
+window.addEventListener("load", () => {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 0) {
+        navbar.classList.add("visible");
+    }
+});
+
+function copyAddress() {
+    const addressInput = document.getElementById("contractAddress").value;
+    navigator.clipboard
+        .writeText(addressInput)
+        .then(() => {
+            const copyButton = document.getElementById("copyButton");
+            copyButton.classList.add("copied");
+            copyButton.textContent = "Copied!";
+
+            setTimeout(() => {
+                copyButton.classList.remove("copied");
+                copyButton.textContent = "Copy";
+            }, 2000);
+        })
+        .catch((err) => {
+            console.error("Could not copy text: ", err);
+        });
+}
+
+function copyAddress2() {
+    const addressInput = document.getElementById("newcontractAddress").value;
+    navigator.clipboard
+        .writeText(addressInput)
+        .then(() => {
+            const copyButton = document.getElementById("copyButton2");
+            copyButton.classList.add("copied");
+            copyButton.textContent = "Copied!";
+
+            setTimeout(() => {
+                copyButton.classList.remove("copied");
+                copyButton.textContent = "Copy";
+            }, 2000);
+        })
+        .catch((err) => {
+            console.error("Could not copy text: ", err);
+        });
+}
